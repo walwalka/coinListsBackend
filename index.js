a2ddd169e1dae9d382cd19397336445e7a930f2c
walwalka <pbballow@gmail.com>
2023-10-11 14:08:16 -0400

    updating routes

--- index.js
+++ index.js
@@ -22,7 +22,9 @@ app.get('/', (request, response) => {
 
 app.use('/coins', coinRoute);
 
-app.use('/coins_new', coinRouteNew)
+app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}`)
+  });
 
 // calling mongoose library for object modeling
 //mongoose
