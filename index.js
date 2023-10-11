f551243923c6b3ec08e502db52f9831c67d061b1
walwalka <pbballow@gmail.com>
2023-10-11 14:09:26 -0400

    removing mongoose and mongo functions

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 import express from 'express';
 import { PORT, PORT2, mongoDBURL } from './config.js';
-import mongoose from 'mongoose';
-import { Coin } from './models/coinModel.js';
-// import coinRoute from './routes/coinRoute.js';
 import coinRoute from './routes/coinRoutePostrges.js';
 import cors from 'cors';
 
@@ -25,17 +22,3 @@ app.use('/coins', coinRoute);
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}`)
   });
-
-// calling mongoose library for object modeling
-//mongoose
-//    .connect(mongoDBURL)
-//    .then(() => {
-//        console.log('App connected to database');
-//        app.listen(PORT, () => {
-//            console.log(`App is listening to port: ${PORT}`);
-//        });
-//        })
-//        .catch((error) => {
-//            console.log(error);
-//        });
-
